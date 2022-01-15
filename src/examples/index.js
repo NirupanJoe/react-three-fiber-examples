6986395d1ab1106a49caff6b203fb8e02a24db9c
NirupanJoe <nirupanjoej@gmail.com>
2022-01-15 14:30:23 +0530

    Introduced a TransparentBox.

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,7 +1,9 @@
 import PositionalAudio from './positionalAudio';
 import SimpleBox from './simpleBox';
+import TransparentBox from './transparentBox';
 
 const examples = {
+	'Transparent Box': TransparentBox,
 	'Simple Box': SimpleBox,
 	'Positional Audio': PositionalAudio,
 };
---
+++ src/examples/transparentBox/index.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-magic-numbers */
+import { Box, Environment, Sphere } from '@react-three/drei';
+import { useControls } from 'leva';
+import React, { Suspense } from 'react';
+
+// eslint-disable-next-line max-lines-per-function
+const control = () => {
+	const bProps = useControls(
+		'TransparentBox', {
+			rotation: [0, 0, 0],
+			position: [0, -1, 0],
+			args: [1, 1, 1],
+			scale: 1,
+			smoothness: 10,
+			radius: 0.015,
+			background: false,
+			preset: {
+				options: {
+					forest: 'forest',
+					sunset: 'sunset',
+					lobby: 'lobby',
+				},
+			},
+		}, { collapsed: true }
+	);
+	const bMProps = useControls(
+		'Box material', {
+			transmission: { value: 0, min: 0, max: 1, step: 0.1 },
+			opacity: { value: 0.5, min: 0, max: 1, step: 0.1 },
+			metalness: { value: 1, min: 0, max: 1, step: 0.1 },
+			roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+			clearcoat: { value: 0, min: 0, max: 1, step: 0.1 },
+			clearcoatRoughness: { value: 0, min: 0, max: 1, step: 0.1 },
+			reflectivity: { value: 0, min: 0, max: 1, step: 0.1 },
+			thickness: { value: 0, min: 0, max: 20, step: 0.5 },
+			envMapIntensity: { value: 0.5, min: 0, max: 10, step: 1 },
+			transparent: true,
+		}, { collapsed: true }
+	);
+	const pProps = useControls(
+		'Sphere', {
+			rotation: [0, 0, 0],
+			position: [0, -1, -2],
+			args: [1, 60, 60],
+			scale: 1,
+		}, { collapsed: true }
+	);
+	const sMProps = useControls(
+		'Sphere material', {
+			transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+			opacity: { value: 1, min: 0, max: 1, step: 0.1 },
+			metalness: { value: 1, min: 0, max: 1, step: 0.1 },
+			roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+			clearcoat: { value: 0, min: 0, max: 1, step: 0.1 },
+			clearcoatRoughness: { value: 0, min: 0, max: 1, step: 0.1 },
+			reflectivity: { value: 0, min: 0, max: 1, step: 0.1 },
+			thickness: { value: 0, min: 0, max: 20, step: 0.5 },
+			envMapIntensity: { value: 1, min: 0, max: 10, step: 1 },
+			ior: { value: 1.25, min: 1, max: 2.3, step: 0.05 },
+			transparent: true,
+			color: '#ffffff',
+		}, { collapsed: true }
+	);
+
+	return { bProps, bMProps, pProps, sMProps };
+};
+
+const TransparentBox = () => {
+	const { bProps, bMProps, pProps, sMProps } = control();
+
+	return (
+		<mesh>
+			<ambientLight/>
+			<directionalLight/>
+			<Suspense fallback={ null }>
+				<Box { ...bProps }>
+					<meshPhysicalMaterial { ...bMProps }/>
+				</Box>
+				<Sphere { ...pProps }>
+					<meshPhysicalMaterial { ...sMProps }/>
+				</Sphere>
+				<Environment preset="sunset"/>
+			</Suspense>
+		</mesh>
+	);
+};
+
+export default TransparentBox;
